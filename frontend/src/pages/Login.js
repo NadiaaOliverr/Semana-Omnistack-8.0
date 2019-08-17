a9d9651323bcea050d727f90c37efd63de61ec89
NadiaaOliverr <nasilvah21@gmail.com>
2019-08-17 01:50:18 -0300

    Adicionando frontend completo da versão web

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
 import './Login.css';
 
+import api from '../services/api';
+
 import logo from '../assets/logo.svg';
 
 export default function Login({ history }) {
     const [username, setUsername] = useState('');
 
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault();
 
-        console.log(username);
-        history.push('/main');
+        const response = await api.post('/devs', {
+            username,
+          });
+
+        const { _id } = response.data;
+
+        history.push(`/dev/${_id}`);
     }
 
     return(
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';  
+import './Main.css'
 
-export default function Main() {
-    return <h1>Hello World</h1>
+import api from '../services/api';
+
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
+export default function Main({ match }) {
+    const [users,setUsers] = useState([]);
+
+    useEffect(() => {
+        async function loadUsers(){
+            const response = await api.get('/devs',{
+                headers: { 
+                    user: match.params.id,
+                }
+            })
+            setUsers(response.data);
+        }
+
+
+        loadUsers();
+    }, [match.params.id] );
+
+    async function handleLike(id){
+        await api.post(`/devs/${id}/likes`,null, {
+            headers: { user:  match.params.id},
+        })
+
+        setUsers(users.filter(user =>  user._id != id));
+    }
+    async function handleDislike(id){
+        await api.post(`/devs/${id}/dislikes`,null, {
+            headers: { user:  match.params.id},
+        })
+
+        setUsers(users.filter(user =>  user._id != id));
+    }
+    return(
+        <div className="main-container">
+            <Link to="/">
+                <img src={logo} alt="Tindev" /> 
+            </Link> 
+                { users.length > 0 ? ( 
+                    <ul>
+                    {users.map(user => (
+                        <li key={user._id}>
+                        <img src={user.avatar} alt={user.name}/>
+                        <footer>
+                            <strong>{user.name}</strong>
+                            <p>{user.bio}</p>
+                        </footer>
+                        <div className="buttons">
+                            <button type="button" onClick={() => handleDislike(user._id)}>
+                                <img src={dislike} alt="Dislike"/>
+                            </button>
+                            <button type="button" onClick={() => handleLike(user._id)}>
+                                <img src={like} alt="Dislike"/>
+                            </button>
+                        </div>
+                        </li>
+                    ))}
+                </ul>
+                ) : (
+                    <div className="empty">Acabou :'( Você está solitário novamente.</div>
+                )}
+                
+        </div>
+    )
 }
